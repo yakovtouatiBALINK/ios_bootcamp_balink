28a2a3f3890fa98bf32ced9e791eb5518ab4a7ee
yakovtouatiBALINK <136307501+yakovtouatiBALINK@users.noreply.github.com>
2023-06-14 13:57:47 +0300

    basic calculator
    the calculator can du the simple calculations

--- week1/day-3/calculator/calculator/ViewController.swift
+++ week1/day-3/calculator/calculator/ViewController.swift
@@ -2,78 +2,92 @@
 //  ViewController.swift
 //  calculator
 //
-//  Created by yacov toua           	                        i on 13/06/2023.
+//  Created by yacov touati on 13/06/2023.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var number: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        number.text = "0"    //  the number that we see in open rhe calculator
     }
     
-    var opera: String = ""
-    var resu: Double?
+    var num1: Double?
+    var num2: Double?
+    var operatorPrees: String?
     
-    
-    @IBOutlet weak var result: UILabel!
-    
-    
+    //function that show the number that pressed
     @IBAction func NumberPressed(_ sender: UIButton) {
-        result.text = sender.titleLabel?.text
+        guard let num = sender.titleLabel?.text else {return}
+        print(num)
+        
+        let currentText = number.text ?? ""
+        if currentText == "0" {
+            number.text = num
+        } else {
+            number.text = currentText + num
+        }
+    }
+    //function that show the operator that pressed
+    @IBAction func operatorPressd(_ sender: UIButton) {
+        guard let operatorText = sender.titleLabel?.text else {return}
+        
+        num1 = Double(number.text ?? "0")
+        operatorPrees = operatorText
+        number.text = operatorPrees
     }
     
+    
+    func plus(_ num1: Double, _ num2: Double) -> Double {
+        return num1 + num2
+    }
+    func minus(_ num1: Double, _ num2: Double) -> Double {
+        return num1 - num2
+    }
+    func multiply(_ num1: Double, _ num2: Double) -> Double {
+        return num1 * num2
+    }
+    func divide(_ num1: Double, _ num2: Double) -> Double? {
+        guard num2 != 0 else {
+            return nil
+        }
+        return num1 / num2
+    }
+    
+    //function that calculate
     @IBAction func equal(_ sender: UIButton) {
-        guard let input =  sender.titleLabel?.text else {return}
-        opera.append(input)
-        print(opera)
-    }
-    @IBAction func point(_ sender: UIButton) {
-        guard let input =  sender.titleLabel?.text else {return}
-        opera.append(input)
-        print(opera)
-    }
-    @IBAction func divide(_ sender: UIButton) {
-        guard let input =  sender.titleLabel?.text else {return}
-        opera.append(input)
-        print(opera)
-    }
-    @IBAction func plus(_ sender: UIButton) {
-        guard let input =  sender.titleLabel?.text else {return}
-        opera.append(input)
-        print(opera)
-    }
-    @IBAction func multiply(_ sender: UIButton) {
-        guard let input =  sender.titleLabel?.text else {return}
-        opera.append(input)
-        print(opera)
-    }
-    @IBAction func minus(_ sender: UIButton) {
-        uard let input =  sender.titleLabel?.text else {return}
-        opera.append(input)
-        print(opera)
+        num2 = Double(number.text ?? "0")
+        
+        if let number1 = num1, let number2 = num2, let operatorText = operatorPrees {
+            var result: Double?
+            switch operatorText {
+            case "+":
+                result = plus(number1, number2)
+            case "-":
+                result = minus(number1, number2)
+            case "*":
+                result = multiply(number1, number2)
+            case "/":
+                result = divide(number1, number2)
+            default:
+                break
+            }
+            if let result = result {
+                number.text = String(result)
+            }
+        }
+        num1 = nil
+        num2 = nil
+        operatorPrees = nil
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //function that delete all screne
+    @IBAction func deleteAll(_ sender: UIButton) {
+        number.text = "0"
+    }
 }
-
+    
